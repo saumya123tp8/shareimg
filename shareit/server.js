e0b3970b0bef5e52856b738622473a1ec1c7b82f
saumya123tp8 <95959848+saumya123tp8@users.noreply.github.com>
2023-09-10 19:46:33 +0530

    Add files via upload

---
+++ shareit/server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const colors = require("colors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const errorHandler = require("./middlewares/errorMiddleware");
+const path=require('path')
+
+//routes path
+const authRoutes = require("./routes/authRoutes");
+
+//dotenv
+dotenv.config();
+
+//mongo connection
+connectDB();
+
+//rest object-- now app has all functinality of express we can use app now
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+// app.use(errorHandler);
+
+app.use(express.static(path.join(__dirname,'./client/build')))
+app.get('*',function(req,res){
+  res.sendFile(path.join(__dirname,'./client/build/index.html'))
+})
+
+//API routes
+app.use("/api/v1/auth", authRoutes);
+// app.use("/api/v1/openai", require("./routes/openaiRoutes"));
+
+//listen server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(
+    `Server Running in ${process.env.DEV_MODE} mode on port no ${PORT}`.bgCyan
+      .white
+  );
+});
